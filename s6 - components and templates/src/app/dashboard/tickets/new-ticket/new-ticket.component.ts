9ee55fb85648e9f08be12c9e451b4d2a3f7bd705
shimako993 <shimako993@gmail.com>
2025-02-26 20:37:27 +0100

    asda

--- s6 - components and templates/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ s6 - components and templates/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, viewChild, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, viewChild, ViewChild } from '@angular/core';
 import { ButtonComponent } from '../../../shared/button/button.component';
 import { ControlComponent } from '../../../shared/control/control.component';
 import { FormsModule } from '@angular/forms';
@@ -10,9 +10,20 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css',
 })
-export class NewTicketComponent {
+export class NewTicketComponent implements OnInit, AfterViewInit {
   // @ViewChild('form') private form?: ElementRef<HTMLFormElement>;
   private form = viewChild.required<ElementRef<HTMLFormElement>>('form');
+
+ngOnInit(): void {
+  console.log('ONINIT')
+  console.log(this.form().nativeElement)
+}
+
+  ngAfterViewInit() {
+      console.log('After VIEW INIT')
+      console.log(this.form().nativeElement)
+  }
+
   onSubmit(title: string, textInput: string) {
     console.log(title, textInput);
     this.form().nativeElement.reset();
