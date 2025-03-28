200ef592250abf7a461418750adc24dae7ad14f1
shimako993 <shimako993@gmail.com>
2025-03-28 18:32:20 +0100

    asda

--- s9-services-deep-dive/src/app/tasks/tasks.service.ts
+++ s9-services-deep-dive/src/app/tasks/tasks.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, signal } from '@angular/core';
-import { Task } from './task.model';
+import { Task, TaskStatus } from './task.model';
 
 @Injectable({
   providedIn: 'root',
@@ -7,14 +7,22 @@ import { Task } from './task.model';
 export class TasksService {
   private tasks = signal<Task[]>([]);
 
-  allTasks = this.tasks.asReadonly()
+  allTasks = this.tasks.asReadonly();
 
-  addTask(taskData: {title: string; description: string}){
+  addTask(taskData: { title: string; description: string }) {
     const newTask: Task = {
-        ...taskData,
-        id:Math.random().toString(),
-        status: 'OPEN'
-    }
-    this.tasks.update((oldTasks)=>[...oldTasks, newTask])
+      ...taskData,
+      id: Math.random().toString(),
+      status: 'OPEN',
+    };
+    this.tasks.update((oldTasks) => [...oldTasks, newTask]);
+  }
+
+  updateTaskStatus(taskId: string, newStatus: TaskStatus) {
+    this.tasks.update((oldTasks) =>
+      oldTasks.map((task) =>
+        task.id === taskId ? { ...task, status: newStatus } : task
+      )
+    );
   }
 }
